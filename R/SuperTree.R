27aed5f9022a4c1f00fca63c19f9d09ead75451e
ahl27 <ahl27@pitt.edu>
2025-05-16 13:17:50 -0400

    fix documentation errors

--- R/SuperTree.R
+++ R/SuperTree.R
@@ -4,17 +4,10 @@ SuperTree <- function(myDendList, NAMEFUN=NULL, Verbose=TRUE, ...){
       !all(vapply(myDendList, \(x) is(x, 'dendrogram') || is.null(x), FUN.VALUE=TRUE))){
     stop("SuperTree requires input to be a list of dendrograms")
   }
-  if(is.null(Processors)){
-    Processors <- detectCores()
-  }
-  stopifnot("'Processors' must be a positive integer"=
-              (Processors >=1 && Processors == as.integer(Processors)))
-  if (!is(Processors, 'integer')) Processors <- as.integer(Processors)
 
   useNFUN <- is(NAMEFUN, 'function')
 
   if (useNFUN){
-    #l1 <- flatdendrapply(myDendList[[1]], LEAFFUN=attr, which='label')
     l1 <- labels(myDendList[[1L]])
     l2 <- NAMEFUN(l1)
     if(length(l1)!=length(l2) || !all(is.atomic(l2))){
@@ -73,27 +66,6 @@ SuperTree <- function(myDendList, NAMEFUN=NULL, Verbose=TRUE, ...){
     cp[] <- 1
     countmat <- combineDist(countmat, cp)
 
-    # d <- as.matrix(Cophenetic(dend))
-    # # Get rownames
-    # if (useNFUN)
-    #   rownames(d) <- colnames(d) <- NAMEFUN(rownames(d))
-    # rn <- rownames(d)
-    # cn <- colnames(d)
-    # lu <- length(unique(rn))
-    # lr <- length(rn)
-    # # Paralogs (currently averaging all rows corresponding to same genome)
-    # while (lu != lr){
-    #   firsttocom <- names(which.max(table(rownames(d)) > 1))
-    #   pos <- which(rownames(d) == firsttocom)
-    #   newrow <- colSums(d[pos,]) / length(pos)
-    #   d[pos[1], ] <- d[,pos[1]] <- newrow
-    #   d <- d[-pos[2], -pos[2]]
-    #   lr <- lr - 1
-    #   rn <- rn[-pos[2]]
-    # }
-    # cn <- rn
-    # dmat[rn, cn] <- dmat[rn, cn] + d
-    # countmat[rn, cn] <- countmat[rn, cn] + 1
     ctr <- ctr + 1
     if (Verbose) setTxtProgressBar(pb, ctr)
   }
